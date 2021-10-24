45f8e3fafd8974a072f18512f9a94051ce0e9bf9
haul73 <ntarasov@rambler.ru>
2021-10-24 19:12:13 +0400

    Добавлена функция поиска окна Nox и установки его в нужную позицию

--- BSW/main.py
+++ BSW/main.py
@@ -1,6 +1,16 @@
-def first(a, b):
-    return a + b
+import pyautogui as pg
+import pprint
+import win32gui
+import win32con
 
 
-if __name__ == '__main__':
-    print(first(2, 3))
+def SetNoxWindow(x=0, y=0, cx=800, cy=400):
+    hwnd = win32gui.FindWindow(None, "NoxPlayer")
+    if hwnd != 0:
+        win32gui.MoveWindow(hwnd, x, y, cx, cy, True)
+        print("Окно установлено")
+    else:
+        print("Окно не найдено")
+
+
+SetNoxWindow()
