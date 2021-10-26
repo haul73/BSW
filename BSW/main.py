ffabbf3ddec8ca94bb9eb574e651a6f3fa665247
haul73 <ntarasov@rambler.ru>
2021-10-27 01:46:14 +0400

    Добавлена функция подсчета hash в выбранной области

--- BSW/main.py
+++ BSW/main.py
@@ -1,16 +1,47 @@
-import pyautogui as pg
-import pprint
 import win32gui
-import win32con
+from PIL import Image, ImageGrab
+import time
+
+box = (0, 0, 1000, 600)
+target = (915, 50, 950, 85)
 
 
-def SetNoxWindow(x=0, y=0, cx=800, cy=400):
+def SetNoxWindow(x):
     hwnd = win32gui.FindWindow(None, "NoxPlayer")
     if hwnd != 0:
-        win32gui.MoveWindow(hwnd, x, y, cx, cy, True)
+        win32gui.MoveWindow(hwnd, x[0], x[1], x[2], x[3], True)
         print("Окно установлено")
     else:
         print("Окно не найдено")
 
 
-SetNoxWindow()
+def makeHash(grab, target):
+    screenshot = ImageGrab.grab(grab)  # Делаем скриншот игры
+    crops = screenshot.crop(target)  # Обрезаем по цели
+    newSize = (8, 8)  # Уменьшаем
+    crops = crops.resize(newSize)
+    crops = crops.convert("L")  # Убираем цвет
+    pix = crops.load()  # Запускаем доступ к пикселям
+    sum = 0  # Выясняем среднюю сумму
+    for i in range(8):
+        for b in range(8):
+            sum = sum + pix[i, b]
+    sum = round(sum / 64)
+    print(sum)
+    hash=""
+    for i in range(8):  # Преобразовываем в  0 и 1
+        for b in range(8):
+            if pix[i, b] >= sum:
+                pix[i, b] = 1
+            else:
+                pix[i, b] = 0
+            hash = hash + str(pix[i, b])
+    hash=hex(int(hash, 2))
+    return hash
+
+
+time.sleep(3)
+SetNoxWindow(box)
+a=makeHash(box, target)
+print(a)
+
